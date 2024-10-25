fbedb8edae6e959c01e751745f60fc5d10dcd4ff
DiorellaM <d.mansillagm@gmail.com>
2024-10-25 13:46:51 -0300

    added post request

--- k6/endpoints/test.js
+++ k6/endpoints/test.js
@@ -1,6 +1,15 @@
 import http from "k6/http";
-import { baseUrl } from "../config/config.js";
+import { sleep } from "k6";
+import { baseUrl, generatePayload, params } from "../config/config.js";
+
+const payload = generatePayload();
+let userRegisterRes;
 
 export default function () {
   http.get(baseUrl);
+
+  const postUserRegister = `${baseUrl}/user/register/`;
+  userRegisterRes = http.post(postUserRegister, payload, params);
+
+  sleep(1);
 }
