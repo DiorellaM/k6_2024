dfd948f88f1d5d01124dbda69e609e7720ab8a4b
DiorellaM <d.mansillagm@gmail.com>
2025-03-11 16:23:32 -0300

    refactor

--- k6/runner/runTests/runTests.js
+++ k6/runner/runTests/runTests.js
@@ -1,5 +1,5 @@
-import test from "../testk6/test.js";
-import userRegister from "../userRegister/userRegister.js";
+import test from "../../endpoints/testk6/test.js";
+import userRegister from "../../endpoints/userRegister/userRegister.js";
 import { quickValidation } from "../../config/shared-iterations-options.js";
 
 export const options = quickValidation;
